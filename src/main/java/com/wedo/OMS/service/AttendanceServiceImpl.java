6648752cce765e09c7acac9ccbb2789694fe8e71
avada218 <34391328+avada218@users.noreply.github.com>
2018-05-18 20:55:54 +0800

    zyp

--- src/main/java/com/wedo/OMS/service/AttendanceServiceImpl.java
+++ src/main/java/com/wedo/OMS/service/AttendanceServiceImpl.java
@@ -2,25 +2,42 @@ package com.wedo.OMS.service;
 
 import com.wedo.OMS.entity.Attendance;
 import com.wedo.OMS.entity.Record;
+import com.wedo.OMS.entity.Task;
+import com.wedo.OMS.entity.User;
+import com.wedo.OMS.repository.TaskRepository;
+import com.wedo.OMS.repository.UserRepository;
 import org.springframework.stereotype.Service;
+import com.wedo.OMS.repository.AttendanceRepository;
+import com.wedo.OMS.repository.RecordRepository;
 
 import java.util.List;
 
 @Service
 public class AttendanceServiceImpl implements AttendanceService {
-
+    private AttendanceRepository attendanceRepository;
+    private RecordRepository recordRepository;
+    private UserRepository userRepository;
+    private TaskRepository taskRepository;
+    public AttendanceServiceImpl(AttendanceRepository attendanceRepository,RecordRepository recordRepository,UserRepository userRepository,TaskRepository taskRepository){
+        this.attendanceRepository =attendanceRepository;
+        this.recordRepository=recordRepository;
+        this.userRepository=userRepository;
+        this.taskRepository=taskRepository;
+    }
     @Override
     public List<Attendance> getAttendancesByUserId(Long userId) {
-        return null;
+        User user = userRepository.findUserById(userId);
+        return attendanceRepository.findAttendancesByUser(user);
     }
 
     @Override
     public List<Attendance> getTaskAttendancesByTaskId(Long taskId) {
-        return null;
+        Task task = taskRepository.findTaskById(taskId);
+        return attendanceRepository.findAttendancesByTask(task);
     }
 
     @Override
     public Record getAttendanceRecordByRecordId(Long recordId) {
-        return null;
+        return recordRepository.findRecordById(recordId);
     }
 }
