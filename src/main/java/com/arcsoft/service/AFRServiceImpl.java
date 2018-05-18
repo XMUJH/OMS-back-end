c527c6a3687edb3016985fef61531c5f32bf7923
XMUJH <948520356@qq.com>
2018-05-18 21:05:07 +0800

    人脸识别

---
+++ src/main/java/com/arcsoft/service/AFRServiceImpl.java
@@ -0,0 +1,370 @@
+package com.arcsoft.service;
+
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.io.InputStream;
+
+import com.arcsoft.AFD_FSDKLibrary;
+import com.arcsoft.AFD_FSDK_FACERES;
+import com.arcsoft.AFD_FSDK_Version;
+import com.arcsoft.AFR_FSDKLibrary;
+import com.arcsoft.AFR_FSDK_FACEINPUT;
+import com.arcsoft.AFR_FSDK_FACEMODEL;
+import com.arcsoft.AFR_FSDK_Version;
+import com.arcsoft.ASVLOFFSCREEN;
+import com.arcsoft.ASVL_COLOR_FORMAT;
+import com.arcsoft.CLibrary;
+import com.arcsoft.FaceInfo;
+import com.arcsoft.MRECT;
+import com.arcsoft._AFD_FSDK_OrientPriority;
+import com.arcsoft.utils.BufferInfo;
+import com.arcsoft.utils.ImageLoader;
+import com.sun.jna.Memory;
+import com.sun.jna.NativeLong;
+import com.sun.jna.Pointer;
+import com.sun.jna.ptr.FloatByReference;
+import com.sun.jna.ptr.PointerByReference;
+
+import org.springframework.stereotype.Service;
+
+@Service
+public class AFRServiceImpl implements AFRService{
+    boolean thirdPartyFace=false;
+
+    @Override
+    public boolean getThirdPartyFace()
+    {
+        return thirdPartyFace;
+    }
+
+    @Override
+    public void setThirdPartyFace(boolean tag)
+    {
+        thirdPartyFace=tag;
+    }
+
+    @Override
+    public String doFR(String inputImg, String[] faceDataPath) {
+        System.out.println("#####################################################");
+        boolean recognitionStatus=false;
+
+        // init Engine
+        Pointer pFDWorkMem = CLibrary.INSTANCE.malloc(FD_WORKBUF_SIZE);
+        Pointer pFRWorkMem = CLibrary.INSTANCE.malloc(FR_WORKBUF_SIZE);
+
+        PointerByReference phFDEngine = new PointerByReference();
+        NativeLong ret = AFD_FSDKLibrary.INSTANCE.AFD_FSDK_InitialFaceEngine(APPID, FD_SDKKEY, pFDWorkMem, FD_WORKBUF_SIZE, phFDEngine, _AFD_FSDK_OrientPriority.AFD_FSDK_OPF_0_HIGHER_EXT, 32, MAX_FACE_NUM);
+        if (ret.longValue() != 0) {
+            CLibrary.INSTANCE.free(pFDWorkMem);
+            CLibrary.INSTANCE.free(pFRWorkMem);
+            System.out.println(String.format("AFD_FSDK_InitialFaceEngine ret 0x%x",ret.longValue()));
+            return String.format("AFD_FSDK_InitialFaceEngine ret 0x%x",ret.longValue());
+        }
+
+        // print FDEngine version
+        Pointer hFDEngine = phFDEngine.getValue();
+        AFD_FSDK_Version versionFD = AFD_FSDKLibrary.INSTANCE.AFD_FSDK_GetVersion(hFDEngine);
+        System.out.println(String.format("%d %d %d %d", versionFD.lCodebase, versionFD.lMajor, versionFD.lMinor, versionFD.lBuild));
+        System.out.println(versionFD.Version);
+        System.out.println(versionFD.BuildDate);
+        System.out.println(versionFD.CopyRight);
+
+        PointerByReference phFREngine = new PointerByReference();
+        ret = AFR_FSDKLibrary.INSTANCE.AFR_FSDK_InitialEngine(APPID, FR_SDKKEY, pFRWorkMem, FR_WORKBUF_SIZE, phFREngine);
+        if (ret.longValue() != 0) {
+            AFD_FSDKLibrary.INSTANCE.AFD_FSDK_UninitialFaceEngine(hFDEngine);
+            CLibrary.INSTANCE.free(pFDWorkMem);
+            CLibrary.INSTANCE.free(pFRWorkMem);
+            System.out.println(String.format("AFR_FSDK_InitialEngine ret 0x%x" ,ret.longValue()));
+            return String.format(String.format("AFR_FSDK_InitialEngine ret 0x%x" ,ret.longValue()));
+        }
+
+        // print FREngine version
+        Pointer hFREngine = phFREngine.getValue();
+        AFR_FSDK_Version versionFR = AFR_FSDKLibrary.INSTANCE.AFR_FSDK_GetVersion(hFREngine);
+        System.out.println(String.format("%d %d %d %d", versionFR.lCodebase, versionFR.lMajor, versionFR.lMinor, versionFR.lBuild));
+        System.out.println(versionFR.Version);
+        System.out.println(versionFR.BuildDate);
+        System.out.println(versionFR.CopyRight);
+
+        // load Image Data
+        ASVLOFFSCREEN inputImgA;
+        ASVLOFFSCREEN inputImgB;
+        inputImgA = loadImage(inputImg);
+        for(int i=0;i<faceDataPath.length;i++)
+        {
+            inputImgB = loadImage(faceDataPath[i]);
+            if(compareFaceSimilarity(hFDEngine, hFREngine, inputImgA, inputImgB)>=0.60)
+            {
+                recognitionStatus=true;
+                break;
+            }
+            if(getThirdPartyFace()==true)
+            {
+                AFD_FSDKLibrary.INSTANCE.AFD_FSDK_UninitialFaceEngine(hFDEngine);
+                AFR_FSDKLibrary.INSTANCE.AFR_FSDK_UninitialEngine(hFREngine);
+
+                CLibrary.INSTANCE.free(pFDWorkMem);
+                CLibrary.INSTANCE.free(pFRWorkMem);
+
+                System.out.println("#####################################################");
+                return "警告！发现第三方人脸";
+            }
+        }
+        // release Engine
+        AFD_FSDKLibrary.INSTANCE.AFD_FSDK_UninitialFaceEngine(hFDEngine);
+        AFR_FSDKLibrary.INSTANCE.AFR_FSDK_UninitialEngine(hFREngine);
+
+        CLibrary.INSTANCE.free(pFDWorkMem);
+        CLibrary.INSTANCE.free(pFRWorkMem);
+
+        System.out.println("#####################################################");
+        if(recognitionStatus==true) return "识别成功";
+        else return ("未能识别该人脸");
+    }
+
+    @Override
+    public FaceInfo[] doFaceDetection(Pointer hFDEngine, ASVLOFFSCREEN inputImg) {
+        FaceInfo[] faceInfo = new FaceInfo[0];
+
+        PointerByReference ppFaceRes = new PointerByReference();
+        NativeLong ret = AFD_FSDKLibrary.INSTANCE.AFD_FSDK_StillImageFaceDetection(hFDEngine, inputImg, ppFaceRes);
+        if (ret.longValue() != 0) {
+            System.out.println(String.format("AFD_FSDK_StillImageFaceDetection ret 0x%x" , ret.longValue()));
+            return faceInfo;
+        }
+
+        AFD_FSDK_FACERES faceRes = new AFD_FSDK_FACERES(ppFaceRes.getValue());
+        if (faceRes.nFace > 0) {
+            faceInfo = new FaceInfo[faceRes.nFace];
+            for (int i = 0; i < faceRes.nFace; i++) {
+                MRECT rect = new MRECT(new Pointer(Pointer.nativeValue(faceRes.rcFace.getPointer()) + faceRes.rcFace.size() * i));
+                int orient = faceRes.lfaceOrient.getPointer().getInt(i * 4);
+                faceInfo[i] = new FaceInfo();
+
+                faceInfo[i].left = rect.left;
+                faceInfo[i].top = rect.top;
+                faceInfo[i].right = rect.right;
+                faceInfo[i].bottom = rect.bottom;
+                faceInfo[i].orient = orient;
+
+                System.out.println(String.format("%d (%d %d %d %d) orient %d", i, rect.left, rect.top, rect.right, rect.bottom, orient));
+            }
+        }
+        return faceInfo;
+    }
+
+    @Override
+    public AFR_FSDK_FACEMODEL extractFRFeature(Pointer hFREngine, ASVLOFFSCREEN inputImg, FaceInfo faceInfo) {
+
+        AFR_FSDK_FACEINPUT faceinput = new AFR_FSDK_FACEINPUT();
+        faceinput.lOrient = faceInfo.orient;
+        faceinput.rcFace.left = faceInfo.left;
+        faceinput.rcFace.top = faceInfo.top;
+        faceinput.rcFace.right = faceInfo.right;
+        faceinput.rcFace.bottom = faceInfo.bottom;
+
+        AFR_FSDK_FACEMODEL faceFeature = new AFR_FSDK_FACEMODEL();
+        NativeLong ret = AFR_FSDKLibrary.INSTANCE.AFR_FSDK_ExtractFRFeature(hFREngine, inputImg, faceinput, faceFeature);
+        if (ret.longValue() != 0) {
+            System.out.println(String.format("AFR_FSDK_ExtractFRFeature ret 0x%x" ,ret.longValue()));
+            return null;
+        }
+
+        try {
+            return faceFeature.deepCopy();
+        } catch (Exception e) {
+            e.printStackTrace();
+            return null;
+        }
+    }
+
+    @Override
+    public float compareFaceSimilarity(Pointer hFDEngine, Pointer hFREngine, ASVLOFFSCREEN inputImgA, ASVLOFFSCREEN inputImgB) {
+        // Do Face Detect
+        FaceInfo[] faceInfosA = doFaceDetection(hFDEngine, inputImgA);
+        if (faceInfosA.length < 1) {
+            System.out.println("no face in Image A ");
+            return 0.0f;
+        }
+
+        FaceInfo[] faceInfosB = doFaceDetection(hFDEngine, inputImgB);
+        if (faceInfosB.length < 1) {
+            System.out.println("no face in Image B ");
+            return 0.0f;
+        }
+
+        // Extract FaceB Feature
+        AFR_FSDK_FACEMODEL faceFeatureB = extractFRFeature(hFREngine, inputImgB, faceInfosB[0]);
+        if (faceFeatureB == null) {
+            System.out.println("extract face feature in Image B failed");
+            faceFeatureB.freeUnmanaged();
+            return 0.0f;
+        }
+        float totValue=0;
+        for(int i=0;i<faceInfosA.length;i++)
+        {
+            // Extract FaceA Feature
+            AFR_FSDK_FACEMODEL faceFeatureA = extractFRFeature(hFREngine, inputImgA, faceInfosA[i]);
+            if (faceFeatureA == null) {
+                System.out.println("extract face feature in Image A failed");
+                faceFeatureA.freeUnmanaged();
+                return 0.0f;
+            }
+
+            // calc similarity between faceA and faceB
+            FloatByReference fSimilScore = new FloatByReference(0.0f);
+            NativeLong ret = AFR_FSDKLibrary.INSTANCE.AFR_FSDK_FacePairMatching(hFREngine, faceFeatureA, faceFeatureB, fSimilScore);
+            faceFeatureA.freeUnmanaged();
+            faceFeatureB.freeUnmanaged();
+            if (ret.longValue() != 0) {
+                System.out.println(String.format("AFR_FSDK_FacePairMatching failed:ret 0x%x" ,ret.longValue()));
+                return 0.0f;
+            }
+            if(fSimilScore.getValue()<0.60&&faceInfosA.length>1)
+            {
+                setThirdPartyFace(true);
+                return fSimilScore.getValue();
+            }
+            else
+            {
+                totValue+=fSimilScore.getValue();
+            }
+        }
+        return totValue/faceInfosA.length;
+    }
+
+    @Override
+    public ASVLOFFSCREEN loadRAWImage(String yuv_filePath, int yuv_width, int yuv_height, int yuv_format) {
+        int yuv_rawdata_size = 0;
+
+        ASVLOFFSCREEN inputImg = new ASVLOFFSCREEN();
+        inputImg.u32PixelArrayFormat = yuv_format;
+        inputImg.i32Width = yuv_width;
+        inputImg.i32Height = yuv_height;
+        if (ASVL_COLOR_FORMAT.ASVL_PAF_I420 == inputImg.u32PixelArrayFormat) {
+            inputImg.pi32Pitch[0] = inputImg.i32Width;
+            inputImg.pi32Pitch[1] = inputImg.i32Width / 2;
+            inputImg.pi32Pitch[2] = inputImg.i32Width / 2;
+            yuv_rawdata_size = inputImg.i32Width * inputImg.i32Height * 3 / 2;
+        } else if (ASVL_COLOR_FORMAT.ASVL_PAF_NV12 == inputImg.u32PixelArrayFormat) {
+            inputImg.pi32Pitch[0] = inputImg.i32Width;
+            inputImg.pi32Pitch[1] = inputImg.i32Width;
+            yuv_rawdata_size = inputImg.i32Width * inputImg.i32Height * 3 / 2;
+        } else if (ASVL_COLOR_FORMAT.ASVL_PAF_NV21 == inputImg.u32PixelArrayFormat) {
+            inputImg.pi32Pitch[0] = inputImg.i32Width;
+            inputImg.pi32Pitch[1] = inputImg.i32Width;
+            yuv_rawdata_size = inputImg.i32Width * inputImg.i32Height * 3 / 2;
+        } else if (ASVL_COLOR_FORMAT.ASVL_PAF_YUYV == inputImg.u32PixelArrayFormat) {
+            inputImg.pi32Pitch[0] = inputImg.i32Width * 2;
+            yuv_rawdata_size = inputImg.i32Width * inputImg.i32Height * 2;
+        } else if (ASVL_COLOR_FORMAT.ASVL_PAF_RGB24_B8G8R8 == inputImg.u32PixelArrayFormat) {
+            inputImg.pi32Pitch[0] = inputImg.i32Width * 3;
+            yuv_rawdata_size = inputImg.i32Width * inputImg.i32Height * 3;
+        } else {
+            System.out.println("unsupported  yuv format");
+            System.exit(0);
+        }
+
+        // load YUV Image Data from File
+        byte[] imagedata = new byte[yuv_rawdata_size];
+        File f = new File(yuv_filePath);
+        InputStream ios = null;
+        try {
+            ios = new FileInputStream(f);
+            ios.read(imagedata,0,yuv_rawdata_size);
+
+        } catch (Exception e) {
+            e.printStackTrace();
+            System.out.println("error in loading yuv file");
+            System.exit(0);
+        } finally {
+            try {
+                if (ios != null) {
+                    ios.close();
+                }
+            } catch (IOException e) {
+            }
+        }
+
+        if (ASVL_COLOR_FORMAT.ASVL_PAF_I420 == inputImg.u32PixelArrayFormat) {
+            inputImg.ppu8Plane[0] = new Memory(inputImg.pi32Pitch[0] * inputImg.i32Height);
+            inputImg.ppu8Plane[0].write(0, imagedata, 0, inputImg.pi32Pitch[0] * inputImg.i32Height);
+            inputImg.ppu8Plane[1] = new Memory(inputImg.pi32Pitch[1] * inputImg.i32Height / 2);
+            inputImg.ppu8Plane[1].write(0, imagedata, inputImg.pi32Pitch[0] * inputImg.i32Height, inputImg.pi32Pitch[1] * inputImg.i32Height / 2);
+            inputImg.ppu8Plane[2] = new Memory(inputImg.pi32Pitch[2] * inputImg.i32Height / 2);
+            inputImg.ppu8Plane[2].write(0, imagedata, inputImg.pi32Pitch[0] * inputImg.i32Height + inputImg.pi32Pitch[1] * inputImg.i32Height / 2, inputImg.pi32Pitch[2] * inputImg.i32Height / 2);
+            inputImg.ppu8Plane[3] = Pointer.NULL;
+        } else if (ASVL_COLOR_FORMAT.ASVL_PAF_NV12 == inputImg.u32PixelArrayFormat) {
+            inputImg.ppu8Plane[0] = new Memory(inputImg.pi32Pitch[0] * inputImg.i32Height);
+            inputImg.ppu8Plane[0].write(0, imagedata, 0, inputImg.pi32Pitch[0] * inputImg.i32Height);
+            inputImg.ppu8Plane[1] = new Memory(inputImg.pi32Pitch[1] * inputImg.i32Height / 2);
+            inputImg.ppu8Plane[1].write(0, imagedata, inputImg.pi32Pitch[0] * inputImg.i32Height, inputImg.pi32Pitch[1] * inputImg.i32Height / 2);
+            inputImg.ppu8Plane[2] = Pointer.NULL;
+            inputImg.ppu8Plane[3] = Pointer.NULL;
+        } else if (ASVL_COLOR_FORMAT.ASVL_PAF_NV21 == inputImg.u32PixelArrayFormat) {
+            inputImg.ppu8Plane[0] = new Memory(inputImg.pi32Pitch[0] * inputImg.i32Height);
+            inputImg.ppu8Plane[0].write(0, imagedata, 0, inputImg.pi32Pitch[0] * inputImg.i32Height);
+            inputImg.ppu8Plane[1] = new Memory(inputImg.pi32Pitch[1] * inputImg.i32Height / 2);
+            inputImg.ppu8Plane[1].write(0, imagedata, inputImg.pi32Pitch[0] * inputImg.i32Height, inputImg.pi32Pitch[1] * inputImg.i32Height / 2);
+            inputImg.ppu8Plane[2] = Pointer.NULL;
+            inputImg.ppu8Plane[3] = Pointer.NULL;
+        } else if (ASVL_COLOR_FORMAT.ASVL_PAF_YUYV == inputImg.u32PixelArrayFormat) {
+            inputImg.ppu8Plane[0] = new Memory(inputImg.pi32Pitch[0] * inputImg.i32Height);
+            inputImg.ppu8Plane[0].write(0, imagedata, 0, inputImg.pi32Pitch[0] * inputImg.i32Height);
+            inputImg.ppu8Plane[1] = Pointer.NULL;
+            inputImg.ppu8Plane[2] = Pointer.NULL;
+            inputImg.ppu8Plane[3] = Pointer.NULL;
+        } else if (ASVL_COLOR_FORMAT.ASVL_PAF_RGB24_B8G8R8 == inputImg.u32PixelArrayFormat) {
+            inputImg.ppu8Plane[0] = new Memory(imagedata.length);
+            inputImg.ppu8Plane[0].write(0, imagedata, 0, imagedata.length);
+            inputImg.ppu8Plane[1] = Pointer.NULL;
+            inputImg.ppu8Plane[2] = Pointer.NULL;
+            inputImg.ppu8Plane[3] = Pointer.NULL;
+        } else {
+            System.out.println("unsupported yuv format");
+            System.exit(0);
+        }
+
+        inputImg.setAutoRead(false);
+        return inputImg;
+    }
+
+    @Override
+    public ASVLOFFSCREEN loadImage(String filePath) {
+        ASVLOFFSCREEN inputImg = new ASVLOFFSCREEN();
+
+        if (bUseBGRToEngine) {
+            BufferInfo bufferInfo = ImageLoader.getBGRFromFile(filePath);
+            inputImg.u32PixelArrayFormat = ASVL_COLOR_FORMAT.ASVL_PAF_RGB24_B8G8R8;
+            inputImg.i32Width = bufferInfo.width;
+            inputImg.i32Height = bufferInfo.height;
+            inputImg.pi32Pitch[0] = inputImg.i32Width * 3;
+            inputImg.ppu8Plane[0] = new Memory(inputImg.pi32Pitch[0] * inputImg.i32Height);
+            inputImg.ppu8Plane[0].write(0, bufferInfo.buffer, 0, inputImg.pi32Pitch[0] * inputImg.i32Height);
+            inputImg.ppu8Plane[1] = Pointer.NULL;
+            inputImg.ppu8Plane[2] = Pointer.NULL;
+            inputImg.ppu8Plane[3] = Pointer.NULL;
+        } else {
+            BufferInfo bufferInfo = ImageLoader.getI420FromFile(filePath);
+            inputImg.u32PixelArrayFormat = ASVL_COLOR_FORMAT.ASVL_PAF_I420;
+            inputImg.i32Width = bufferInfo.width;
+            inputImg.i32Height = bufferInfo.height;
+            inputImg.pi32Pitch[0] = inputImg.i32Width;
+            inputImg.pi32Pitch[1] = inputImg.i32Width / 2;
+            inputImg.pi32Pitch[2] = inputImg.i32Width / 2;
+            inputImg.ppu8Plane[0] = new Memory(inputImg.pi32Pitch[0] * inputImg.i32Height);
+            inputImg.ppu8Plane[0].write(0, bufferInfo.buffer, 0, inputImg.pi32Pitch[0] * inputImg.i32Height);
+            inputImg.ppu8Plane[1] = new Memory(inputImg.pi32Pitch[1] * inputImg.i32Height / 2);
+            inputImg.ppu8Plane[1].write(0, bufferInfo.buffer, inputImg.pi32Pitch[0] * inputImg.i32Height, inputImg.pi32Pitch[1] * inputImg.i32Height / 2);
+            inputImg.ppu8Plane[2] = new Memory(inputImg.pi32Pitch[2] * inputImg.i32Height / 2);
+            inputImg.ppu8Plane[2].write(0, bufferInfo.buffer, inputImg.pi32Pitch[0] * inputImg.i32Height + inputImg.pi32Pitch[1] * inputImg.i32Height / 2, inputImg.pi32Pitch[2] * inputImg.i32Height / 2);
+            inputImg.ppu8Plane[3] = Pointer.NULL;
+        }
+
+        inputImg.setAutoRead(false);
+        return inputImg;
+    }
+
+}
--- src/main/java/com/wedo/OMS/controller/UserController.java
+++ src/main/java/com/wedo/OMS/controller/UserController.java
@@ -8,20 +8,26 @@ import com.wedo.OMS.repository.TaskRepository;
 import com.wedo.OMS.repository.UserRepository;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.context.annotation.ComponentScan;
+
+import com.arcsoft.service.AFRService;
 
 import java.util.List;
 
 @RestController
+@ComponentScan(value="com.arcsoft")
 public class UserController {
     private UserRepository userRepository;
     private MilestoneRepository milestoneRepository;
     private TaskRepository taskRepository;
+    private AFRService afrService;
 
-    public UserController(UserRepository userRepository, MilestoneRepository milestoneRepository, TaskRepository taskRepository) {
+    public UserController(UserRepository userRepository, MilestoneRepository milestoneRepository, TaskRepository taskRepository, AFRService afrService) {
 
         this.userRepository = userRepository;
         this.milestoneRepository = milestoneRepository;
         this.taskRepository = taskRepository;
+        this.afrService = afrService;
     }
 
     @GetMapping(value = "/user")
@@ -45,6 +51,10 @@ public class UserController {
         return milestoneRepository.findAll();
     }
 
+    @GetMapping(value = "/facetest")
+    public String faceTest() {
+        return afrService.doFR("src/main/resources/static/004.png", new String[]{"src/main/resources/static/faceimg/001.jpg","src/main/resources/static/faceimg/002.jpg","src/main/resources/static/faceimg/003.jpg"});
+    }
     /*@DeleteMapping(value = "/user")
     public boolean deleteUser(long userId) {
         userRepository.delete();
