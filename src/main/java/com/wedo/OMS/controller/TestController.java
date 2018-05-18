79512db072e3e8eafa3880ad6aba8e69c21eae42
XMUJH <948520356@qq.com>
2018-05-18 21:59:46 +0800

    3

--- src/main/java/com/wedo/OMS/controller/TestController.java
+++ src/main/java/com/wedo/OMS/controller/TestController.java
@@ -1,7 +1,6 @@
 package com.wedo.OMS.controller;
 
 import com.arcsoft.service.AFRService;
-
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.context.annotation.ComponentScan;
@@ -13,6 +12,7 @@ import com.wedo.OMS.repository.CompanyRepository;
 import com.wedo.OMS.repository.RecordRepository;
 import com.wedo.OMS.service.AttendanceService;
 import com.wedo.OMS.service.CompanyService;
+
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -21,24 +21,16 @@ import java.util.List;
 @RestController
 @ComponentScan(value="com.arcsoft")
 public class TestController {
-    private AFRService afrService;
-
-    public TestController(AFRService afrService) {
-        this.afrService = afrService;
-    }
-
-    @GetMapping(value = "/facetest")
-    public String faceTest() {
-        return afrService.doFR("src/main/resources/static/004.png", new String[]{"src/main/resources/static/faceimg/001.jpg","src/main/resources/static/faceimg/002.jpg","src/main/resources/static/faceimg/003.jpg"});
-public class TestController {
     private AttendanceService attendanceService;
     private CompanyService companyService;
     private RecordRepository recordRepository;
+    private AFRService afrService;
 
-    public TestController(AttendanceService attendanceService, RecordRepository recordRepository, CompanyService companyService) {
+    public TestController(AttendanceService attendanceService, RecordRepository recordRepository, CompanyService companyService, AFRService afrService) {
        this.attendanceService = attendanceService;
        this.recordRepository = recordRepository;
        this.companyService = companyService;
+        this.afrService = afrService;
     }
 
     /*AttendanceService*/
@@ -123,5 +115,10 @@ public class TestController {
         long companyid=1;
         long userid=1;
         companyService.deleteCompanyUser(companyid,userid);
+    }
+
+    @GetMapping(value = "/facetest")
+    public String faceTest() {
+        return afrService.doFR("src/main/resources/static/004.png", new String[]{"src/main/resources/static/faceimg/001.jpg", "src/main/resources/static/faceimg/002.jpg", "src/main/resources/static/faceimg/003.jpg"});
     }
 }
