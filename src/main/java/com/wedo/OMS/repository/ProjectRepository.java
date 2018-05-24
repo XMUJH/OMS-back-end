96daa5798660f38339dd6d81c17af013c1c3f0a8
avada218 <34391328+avada218@users.noreply.github.com>
2018-05-24 15:51:49 +0800

    添加projectController中获取project子目录

--- src/main/java/com/wedo/OMS/repository/ProjectRepository.java
+++ src/main/java/com/wedo/OMS/repository/ProjectRepository.java
@@ -3,6 +3,9 @@ package com.wedo.OMS.repository;
 import com.wedo.OMS.entity.Project;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+
 public interface ProjectRepository extends JpaRepository<Project, Long> {
     Project findProjectById(long projectId);
+    List<Project> findProjectsByBelong(Project project);
 }
