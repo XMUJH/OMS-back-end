8ffee5580d289807e525b0434dba6ca30b706356
eve <609490912@qq.com>
2018-05-25 18:08:46 +0800

    人脸识别

--- src/main/java/com/wedo/OMS/controller/UserController.java
+++ src/main/java/com/wedo/OMS/controller/UserController.java
@@ -19,7 +19,6 @@ import org.slf4j.LoggerFactory;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.multipart.MultipartFile;
 
-import java.io.IOException;
 import java.util.List;
 
 @RestController
@@ -79,7 +78,6 @@ public class UserController {
         return milestoneRepository.findAll();
     }
 
-    //TODO Should be removed
     @PostMapping(value = "/faceRecognition/{userId}")
     public @ResponseBody
     int faceRecognition(@RequestParam("img") MultipartFile file, @PathVariable("userId") long userId) throws UserNotFoundException {
