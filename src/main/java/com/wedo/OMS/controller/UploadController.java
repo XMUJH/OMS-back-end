dfe9fcfe986c0830106620f46306d875735e0290
eve <609490912@qq.com>
2018-05-29 10:53:14 +0800

    上传资源

--- src/main/java/com/wedo/OMS/controller/UploadController.java
+++ src/main/java/com/wedo/OMS/controller/UploadController.java
@@ -1,5 +1,7 @@
 package com.wedo.OMS.controller;
 
+import com.wedo.OMS.entity.Resource;
+import com.wedo.OMS.service.ResourceService;
 import com.wedo.OMS.utils.FileUtil;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -9,11 +11,16 @@ import org.springframework.web.multipart.MultipartFile;
 @RestController
 public class UploadController {
     private final Logger logger = LoggerFactory.getLogger(UploadController.class);
+    private ResourceService resourceService;
+
+    public UploadController(ResourceService resourceService) {
+        this.resourceService = resourceService;
+    }
 
     //TODO Should not upload files to project folder
     @RequestMapping(value = "/upload/resources", method = RequestMethod.POST)
     public @ResponseBody
-    String uploadResource(@RequestParam("file") MultipartFile file) {
+    long uploadResource(@RequestParam("file") MultipartFile file) {
         String fileName = file.getOriginalFilename();
         String filePath = "src/main/resources/static/resource/";
         try {
@@ -21,10 +28,11 @@ public class UploadController {
         } catch (Exception e) {
             e.printStackTrace();
             logger.error(e.getMessage());
-            return "upload fail";
+            return -1;
         }
         //TODO Consider to return status code and message
-        return "upload success";
+        Resource resource = resourceService.addResource(fileName);
+        return resource.getId();
     }
 
     @RequestMapping(value = "/upload/results", method = RequestMethod.POST)
